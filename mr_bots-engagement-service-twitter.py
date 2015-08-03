e0d22a9712668be77475015e8e0559b6ec3be757
Mary Burford <mburford@paperlesspost.net>
2015-08-02 21:16:16 -0400

    refactor service to connect to pg, and ... insert .. biggie data

--- mr_bots-engagement-service-twitter.py
+++ mr_bots-engagement-service-twitter.py
@@ -1,9 +1,10 @@
 import tweepy
-import sqlite3
+import psycopg2
 import collections
 import random
+import time
 from time import sleep
-
+import datetime
 # from the campaign target's results, pick random prey
 def random_prey(prey, max_favs):
 	prey = prey
@@ -17,17 +18,22 @@ def random_prey(prey, max_favs):
 
 
 # connect to mr_bots mr_database
-conn = sqlite3.connect('/Users/mary/mr_bots-webapp/db/development.sqlite3') 
+conn = psycopg2.connect("dbname='development' user='mary' host='localhost'")
 c = conn.cursor()
-# fetch all active mr_campaigns
-c.execute('select c.id, c.target, c.max_favs, a.token, a.secret, a.id from campaigns c join accounts a on c.account_id = a.id where c.active = "t" and a.id = "1"')
+print c
+# fetch all active mr_campaigns (will need to change to get all active campaigns)
+c.execute('select c.id, c.target, c.engagements_per_prey, a.token, a.secret, a.id from campaigns c left join accounts a on c.account_id = a.id')
+
 campaigns = c.fetchall()
+for r in campaigns:
+	print r
+print campaigns
 
 # iterate through mr_campaigns and create the mr_engagement_queue
 engagements = dict()
-for c in campaigns:
+for camp in campaigns:
 	engagements_for_campaign = collections.deque()
-	campaign_id, campaign_target, max_favs, token, secret, account_id = c
+	campaign_id, campaign_target, max_favs, token, secret, account_id = camp
 	auth = tweepy.OAuthHandler('tdGB5bGdjqlM3hRVIA3VYY0n9', 'vaAejiob0uko8YPu81tTxB585cvA4G1WmKmwGLGESpMOw5MXxr')
 	auth.set_access_token(token, secret)
 	api = tweepy.API(auth)
@@ -54,6 +60,8 @@ while not_done:
 		for gag in engage:
 			prey_id = gag
 			campaign_id = engage[gag][0]
+			print 'prey id: '+str(prey_id)
+			print 'campaign id: '+str(campaign_id)
 			token = engage[gag][1]
 			secret = engage[gag][2]
 			# construct api connection
@@ -62,6 +70,7 @@ while not_done:
 			api = tweepy.API(auth)
 			# get tweets
 			tweets = api.user_timeline(prey_id)
+			# print tweets
 			# iterate through tweets
 			for t in tweets:
 				print t.id
@@ -69,14 +78,20 @@ while not_done:
 				user_name = json_object['user']['screen_name']
 				tweet_id = t.id
 				tweet_lookup = str(campaign_id) + ' ' + str(tweet_id)
+				print tweet_lookup
 				# check to see if prey has already been engaged
 				if tweet_lookup in engaged_tweets:
 					pass
-				# engage if prey needs engagement	
+				# engage if prey needs engagement and log 	
 				else:
 					api.create_favorite(tweet_id)
-					faved_tweets.append(tweet_lookup)
-			sleep(90)
+					engaged_tweets.append(tweet_lookup)
+					print 'prey id: '+str(prey_id)
+					print 'campaign id: '+str(campaign_id)
+					print c
+					c.execute("INSERT INTO engagements (campaign_id, prey_id, created_at, updated_at) VALUES ('{campaign_id}', '{prey_id}', '{created_at}', '{updated_at}')".format(campaign_id=campaign_id, prey_id=prey_id, created_at=datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S'), updated_at=datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')))
+					conn.commit()
+					sleep(90)
 		if len(engagement_queue) > 0:
 			not_done = True
 
