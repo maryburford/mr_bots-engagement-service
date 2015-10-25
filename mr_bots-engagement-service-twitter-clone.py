1db85a9d2009e7da69c24b5d105d571001349b7b
Mary Burford <mburford@paperlesspost.net>
2015-10-25 19:51:40 -0400

    Found some markov chain code on github. Integrated it into this script

--- mr_bots-engagement-service-twitter-clone.py
+++ mr_bots-engagement-service-twitter-clone.py
@@ -8,8 +8,118 @@ from time import sleep
 import datetime
 import argparse
 from pprint import pprint
+import re
 
 tweet_texts = []
+tempMapping = {}
+
+# (tuple of words) -> {dict: word -> *normalized* number of times the word appears following the tuple}
+# Example entry:
+#    ('eyes', 'turned') => {'to': 0.66666666, 'from': 0.33333333}
+mapping = {}
+
+# Contains the set of words that can start sentences
+starts = []
+
+# We want to be able to compare words independent of their capitalization.
+def fixCaps(word):
+    # Ex: "FOO" -> "foo"
+    if word.isupper() and word != "I":
+        word = word.lower()
+        # Ex: "LaTeX" => "Latex"
+    elif word [0].isupper():
+        word = word.lower().capitalize()
+        # Ex: "wOOt" -> "woot"
+    else:
+        word = word.lower()
+    return word
+
+def toHashKey(lst):
+	    return tuple(lst)
+
+	# Returns the contents of the file, split into a list of words and
+	# (some) punctuation.
+def wordlist(tweet_texts):
+    
+    wordlist = [fixCaps(w) for w in re.findall(r"[\w']+|[.,!?;]", tweet_texts)]
+    return wordlist
+
+# Self-explanatory -- adds "word" to the "tempMapping" dict under "history".
+# tempMapping (and mapping) both match each word to a list of possible next
+# words.
+# Given history = ["the", "rain", "in"] and word = "Spain", we add "Spain" to
+# the entries for ["the", "rain", "in"], ["rain", "in"], and ["in"].
+def addItemToTempMapping(history, word):
+    global tempMapping
+    while len(history) > 0:
+        first = toHashKey(history)
+        if first in tempMapping:
+            if word in tempMapping[first]:
+                tempMapping[first][word] += 1.0
+            else:
+                tempMapping[first][word] = 1.0
+        else:
+            tempMapping[first] = {}
+            tempMapping[first][word] = 1.0
+        history = history[1:]
+
+# Building and normalizing the mapping.
+def buildMapping(wordlist, markovLength):
+    global tempMapping
+    starts.append(wordlist [0])
+    for i in range(1, len(wordlist) - 1):
+        if i <= markovLength:
+            history = wordlist[: i + 1]
+        else:
+            history = wordlist[i - markovLength + 1 : i + 1]
+        follow = wordlist[i + 1]
+        # if the last elt was a period, add the next word to the start list
+        if history[-1] == "." and follow not in ".,!?;":
+            starts.append(follow)
+        addItemToTempMapping(history, follow)
+    # Normalize the values in tempMapping, put them into mapping
+    for first, followset in tempMapping.iteritems():
+        total = sum(followset.values())
+        # Normalizing here:
+        mapping[first] = dict([(k, v / total) for k, v in followset.iteritems()])
+
+# Returns the next word in the sentence (chosen randomly),
+# given the previous ones.
+def next(prevList):
+    sum = 0.0
+    retval = ""
+    index = random.random()
+    # Shorten prevList until it's in mapping
+    while toHashKey(prevList) not in mapping:
+    	try: 
+        	prevList.pop(0)
+        except:
+        	pass
+    # Get a random word from the mapping, given prevList
+    for k, v in mapping[toHashKey(prevList)].iteritems():
+        sum += v
+        if sum >= index and retval == "":
+            retval = k
+    return retval
+
+def genSentence(markovLength):
+    # Start with a random "starting word"
+    curr = random.choice(starts)
+    sent = curr.capitalize()
+    prevList = [curr]
+    # Keep adding words until we hit a period
+    while (curr not in "."):
+        curr = next(prevList)
+        prevList.append(curr)
+        # if the prevList has gotten too long, trim it
+        if len(prevList) > markovLength:
+            prevList.pop(0)
+        if (curr not in ".,!?;"):
+            sent += " " # Add spaces between words (but not punctuation)
+        sent += curr
+    return sent
+
+
 def clean_tweets(new_tweets):
 	for i in new_tweets:
 		keys = i.keys()
@@ -23,6 +133,9 @@ def clean_tweets(new_tweets):
 			tweet_texts.append(tweet_text)
 			oldest = i['id']
 	return oldest
+
+
+
 
 def build_corpus(consumer_key, consumer_secret, target, access_key, access_secret):
 	# this builds a corpus of at least the most recent 100 tweets
@@ -40,7 +153,17 @@ def build_corpus(consumer_key, consumer_secret, target, access_key, access_secre
 		new_tweets = api.user_timeline(screen_name = target ,count=200,max_id=oldest,exclude_replies=True,include_rts=False)
 		oldest = clean_tweets(new_tweets)
 
+ 
 
+
+
+
+def generateTweets(consumer_key, consumer_secret, target, access_key, access_secret):
+	markovLength = 1
+	build_corpus(consumer_key, consumer_secret, target, access_key, access_secret)
+	words = " ".join(str(x) for x in tweet_texts)
+	buildMapping(wordlist(words), markovLength)
+	print genSentence(markovLength)
 
 if __name__  == "__main__":
 	parser = argparse.ArgumentParser(description="Twitter engagement service.")
@@ -59,8 +182,7 @@ if __name__  == "__main__":
 	# if args.consumer_key and args.consumer_secret and args.pg_user and args.pg_password and args.pg_db:
 	#	engage(args.consumer_key, args.consumer_secret, args.pg_user, args.pg_password, args.pg_db, args.pg_host)
 	if args.consumer_key and args.consumer_secret:
-		print 'fucks'
-		build_corpus(args.consumer_key, args.consumer_secret, args.target, args.access_key, args.access_secret)
+		generateTweets(args.consumer_key, args.consumer_secret, args.target, args.access_key, args.access_secret)
 	else:	
 		print "Specify all arguments."
   
